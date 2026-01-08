cdda73d1d874ca31b236c2eef6ae311118fb0eb5
philipsuh004 <157208553+philipsuh004@users.noreply.github.com>
2026-01-08 10:09:48 -0800

    start

---
+++ build/src/CMakeFiles/ray_tracing.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ray_tracing.dir/main.cc.o"
+  "CMakeFiles/ray_tracing.dir/main.cc.o.d"
+  "ray_tracing"
+  "ray_tracing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ray_tracing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
