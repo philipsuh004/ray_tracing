f555f003c7837e3c64852f3f0e2ac0f8459ed10d
philipsuh004 <157208553+philipsuh004@users.noreply.github.com>
2026-01-08 23:46:34 -0800

    some header files

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,6 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
 using namespace std;
@@ -14,16 +17,13 @@ int main() {
     cout << "P3\n" << image_width << ' ' << image_height << "\n355\n";
 
     for (int j = 0; j < image_height; j++) {
+        clog << "\rScanlines remaining: " << (image_height - j) << ' ' << flush;
         for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            cout << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+            write_color(cout, pixel_color);
         }
     }
+
+    clog << "\rDone.                 \n";
+
 }
