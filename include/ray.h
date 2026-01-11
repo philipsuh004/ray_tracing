b4ba5872e4e2d3dbd7f02c6a078bed3ee5496b64
philipsuh004 <157208553+philipsuh004@users.noreply.github.com>
2026-01-11 12:07:22 -0800

    shading with normals

--- include/ray.h
+++ include/ray.h
@@ -11,10 +11,14 @@ class ray {
 
         const point3& origin() const { return orig; }
         const vec3& direction() const {return dir; }
-        
-        private:
-            point3 orig;
-            vec3 dir;
+
+        point3 at(double t) const {
+            return orig + t*dir;
+        }
+
+    private:
+        point3 orig;
+        vec3 dir;
 };
 
 #endif
