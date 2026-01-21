5aa31cc3653d467041ca35da42cfec1a98d7d862
philipsuh004 <157208553+philipsuh004@users.noreply.github.com>
2026-01-20 20:30:55 -0800

    ray depth

--- include/camera.h
+++ include/camera.h
@@ -9,6 +9,7 @@ class camera {
         double aspect_ratio = 1.0; // image width over height
         int image_width = 100; // width in pixels
         int samples_per_pixel = 10; // random samples per pixel
+        int max_depth = 10; // max ray bounces
 
         void render(const hittable& world) {
             initialize();
@@ -21,7 +22,7 @@ class camera {
                     color pixel_color(0,0,0);
                     for (int sample = 0; sample < samples_per_pixel; sample++) {
                         ray r = get_ray(i, j);
-                        pixel_color += ray_color(r, world);
+                        pixel_color += ray_color(r, max_depth, world);
                     }
                     write_color(std::cout, pixel_samples_scale * pixel_color);
 
@@ -85,12 +86,17 @@ class camera {
                 return vec3(random_double() - 0.5, random_double() - 0.5, 0);
             }
 
-            color ray_color(const ray& r, const hittable& world) const {
+            color ray_color(const ray& r, int depth, const hittable& world) const {
+                // no light if exceed bounce limit
+                if (depth <= 0) {
+                    return color(0,0,0);
+                }
+
                 hit_record rec;
 
-                if (world.hit(r, interval(0, infinity), rec)) {
+                if (world.hit(r, interval(0.001, infinity), rec)) {
                     vec3 direction = random_on_hemisphere(rec.normal);
-                    return 0.5 * ray_color(ray(rec.p, direction), world);    
+                    return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);    
                 }
 
                 vec3 unit_direction = unit_vector(r.direction());
